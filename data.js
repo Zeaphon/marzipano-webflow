350a311338da8bec775d7c802e96a28f9eb776c2
Tim <zephon22@web.de>
2024-06-12 12:35:27 +0200

    Upload Marzipano Main Files

---
+++ data.js
@@ -0,0 +1,357 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-oak-tec-kche",
+      "name": "Oak Tec Küche",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "yaw": -1.1679236328617861,
+        "pitch": 0.1512991899067888,
+        "fov": 1.339867637530951
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.0877732427195603,
+          "pitch": 0.08934763951236491,
+          "rotation": 5.497787143782138,
+          "target": "1-oak-tec-meeting-room"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-oak-tec-meeting-room",
+      "name": "Oak Tec Meeting Room",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "yaw": -0.0012407553924145276,
+        "pitch": 0.13190694909227219,
+        "fov": 1.339867637530951
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.098279519832098,
+          "pitch": 0.014164022911945295,
+          "rotation": 5.497787143782138,
+          "target": "2-oak-tec-open-office"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-oak-tec-open-office",
+      "name": "Oak Tec Open Office",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "yaw": 2.2293407388689053,
+        "pitch": 0.07682718562868018,
+        "fov": 1.339867637530951
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.46234234655353745,
+          "pitch": 0.07726120203786735,
+          "rotation": 4.71238898038469,
+          "target": "1-oak-tec-meeting-room"
+        },
+        {
+          "yaw": -1.5393745305294857,
+          "pitch": 0.0007579360484051278,
+          "rotation": 0,
+          "target": "0-oak-tec-kche"
+        },
+        {
+          "yaw": -0.012312537888467645,
+          "pitch": 0.09614841528899731,
+          "rotation": 0,
+          "target": "3-oak-tec-ping-pong"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-oak-tec-ping-pong",
+      "name": "Oak Tec Ping Pong",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "yaw": -0.4950614015727943,
+        "pitch": 0.22215421969425542,
+        "fov": 1.339867637530951
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.820184592485056,
+          "pitch": 0.07833466886419771,
+          "rotation": 14.137166941154074,
+          "target": "0-oak-tec-kche"
+        },
+        {
+          "yaw": 2.8140856444625157,
+          "pitch": 0.08354140838828172,
+          "rotation": 4.71238898038469,
+          "target": "2-oak-tec-open-office"
+        },
+        {
+          "yaw": -1.8638754732430236,
+          "pitch": 0.07785845580691131,
+          "rotation": 0,
+          "target": "1-oak-tec-meeting-room"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-oak-tec-production-floor",
+      "name": "Oak Tec Production Floor",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.339867637530951
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.09221147393624207,
+          "pitch": 0.0011181031798059848,
+          "rotation": 0,
+          "target": "5-oak-tec-production-floor-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-oak-tec-production-floor-2",
+      "name": "Oak Tec Production Floor 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "yaw": -2.3542699789396764,
+        "pitch": 0.07374818673623196,
+        "fov": 1.339867637530951
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.0159332787031286,
+          "pitch": 0.023722169634297074,
+          "rotation": 0,
+          "target": "4-oak-tec-production-floor"
+        },
+        {
+          "yaw": 0.9859404879336342,
+          "pitch": 0.03927248672782824,
+          "rotation": 0,
+          "target": "6-oak-tec-production-floor-3"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-oak-tec-production-floor-3",
+      "name": "Oak Tec Production Floor 3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "yaw": -2.112210147845442,
+        "pitch": 0.07810109286126377,
+        "fov": 1.339867637530951
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.0907868334569635,
+          "pitch": 0.10937817958448548,
+          "rotation": 0,
+          "target": "5-oak-tec-production-floor-2"
+        },
+        {
+          "yaw": -2.7963131162263988,
+          "pitch": 0.059812101846398846,
+          "rotation": 0,
+          "target": "4-oak-tec-production-floor"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-oak-tec-production-floor-4",
+      "name": "Oak Tec Production Floor 4",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "yaw": -2.401465402129766,
+        "pitch": -0.18459542251208916,
+        "fov": 1.339867637530951
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.1283409616869076,
+          "pitch": 0.05090583125220327,
+          "rotation": 0,
+          "target": "5-oak-tec-production-floor-2"
+        },
+        {
+          "yaw": -2.464373239011705,
+          "pitch": 0.01734645695892212,
+          "rotation": 0,
+          "target": "4-oak-tec-production-floor"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Oak Tec 360° Tour",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
